d4435237615537fa2b3a638fb235a4925fa815d7
takafumi.shimomura <takafumi.shimomura@uni.iret.co.jp>
2023-01-27 14:58:10 +0900

    add new file

---
+++ vpc.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_version = "~> 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "s3" {
+    encrypt        = true
+    bucket         = "shimomura-test-codebuild"
+    region         = "ap-northeast-1"
+  }
+}
+
+provider "aws" {
+  region  = "ap-northeast-1"
+}
+
+
+resource "aws_vpc" "shimomura" {
+  cidr_block           = "shimomura-test"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "shimomura"
+  }
+}
